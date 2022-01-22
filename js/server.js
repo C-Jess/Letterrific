9afb8208196eb6634bc82b49c7ab0069b5ccd2e8
Jessica Christensen <65625958+C-Jess@users.noreply.github.com>
2022-01-22 23:48:48 +0000

    Initial commit

---
+++ js/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+var token = process.env.TOKEN;
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
+      { expires_in: 3600 }, // can set a TTL timer in seconds.
+      { headers: { authorization: token } }); // AssemblyAI API Key goes here
+    const { data } = response;
+    res.json(data);
+  } catch (error) {
+    const {response: {status, data}} = error;
+    res.status(status).json(data);
+  }
+});
+
+app.set('port', 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${server.address().port}`);
+});
